1d121dd86a06007930cb9d603ba0acc979b74aa4
zrh <17671730182@163.com>
2025-03-31 22:20:52 +0800

    redis配置

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -5,7 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 
 /**
  * @author zrh
@@ -20,10 +21,18 @@ public class RedisConfiguration {
     @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始配置RedisTemplate");
-        RedisTemplate redisTemplate = new RedisTemplate();
+//		1.创建 redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+//		2.设置连接工厂
         redisTemplate.setConnectionFactory(redisConnectionFactory);
-        //设置序列化器
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        return redisTemplate;
+//		3.设置序列化工具
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+//		key 和 hashkey 采用 String 序列化
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+//		value 和 hashvalue 采用 json 序列化
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+        return  redisTemplate;
     }
 }
